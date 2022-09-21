e359699e4fae978aadc21ca1acda1fbf8d017a61
JeanPaul-C <98408929+JeanPaul-C@users.noreply.github.com>
2022-09-21 17:40:14 -0400

    Update basic.cy.js

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -5,7 +5,7 @@ describe('sample test', () => {
 
   it('displays the resources text', () => {
     cy.get('h1')
-    .contains('Welcome to my app!');
+    .contains('Welcome to my app! Page made by Jeanpaul Choi!');
   })
   it('renders the Netlify logo image', () => {
     cy.get('img')
