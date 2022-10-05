c3b50d51ac5c841ff35322843240df55d6631877
JeanPaul-C <98408929+JeanPaul-C@users.noreply.github.com>
2022-10-05 16:59:01 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,23 +4,20 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-  
-      <main className={styles.main}>
-        E2065767 - Jean-paul Choi - Patrick Pierre - 420-5D2-MA Applications web 2
+    <div className="container">
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="E2065767 - Jean-paul Choi - Patrick Pierre - 420-5D2-MA Applications web 2" />
+        <p className="description">
+          Get started by editing <code>pages/index.js</code>
+        </p>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer">
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <Footer />
     </div>
   )
 }
